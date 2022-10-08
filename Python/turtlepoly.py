4e0f183b2df19a5e15e29b6f7ee0db1800957078
Rohit-cyber696 <69785675+Rohit-cyber696@users.noreply.github.com>
2022-10-08 17:28:39 +0530

    Create turtlepoly.py
    To draw polygons of different shapes using python turtle

---
+++ Python/turtlepoly.py
@@ -0,0 +1,10 @@
+#polygon
+
+import turtle
+p = turtle.Turtle()
+s = int(input("enter sides"))
+l = int(input("Enetr side length"))
+for i in range(s):
+    p.forward(l)
+    p.left(360/s)
+turtle.done()
