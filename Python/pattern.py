cdd5238e43de10a0a33fa36141191bdee2d6f902
Nimisha Anna Binoy <74232971+nimisha-1234-binoy@users.noreply.github.com>
2022-10-13 14:31:13 +0530

    Create pattern.py
    python program to print a pattern

---
+++ Python/pattern.py
@@ -0,0 +1,10 @@
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(" ",end="")
+    for k in range(1,2*i-1+1):
+        if(k==1 or k==2*i-1 or i==n):
+            print(chr(ord('A')),end="")
+        else:
+            print(" ",end="")
+    print()
